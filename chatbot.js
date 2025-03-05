fa94ca92886822299a2c9e29fdeee11f8dc11261
schmidtronaldo <schmidt_dev_2024@outlook.com>
2025-03-05 10:42:30 -0300

    Refatorando: versão:2.1

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,12 @@
 // Importações
-const qrcode = require('qrcode-terminal');
-const { Client } = require('whatsapp-web.js');
+import qrcode from 'qrcode-terminal';
+import { Client } from 'whatsapp-web.js';
+import fs from 'fs';
+
+// Carrega as mensagens do arquivo JSON
+const messages = JSON.parse(fs.readFileSync('./messages.json', 'utf-8'));
+
+// Inicializa o cliente
 const client = new Client();
 
 // Função para simular digitação
@@ -16,56 +22,38 @@ const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 const responses = {
   '1': async (msg) => {
     const chat = await msg.getChat();
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(
-      msg.from,
-      'Nosso serviço oferece consultas médicas 24 horas por dia, 7 dias por semana, diretamente pelo WhatsApp.\n\nNão há carência, o que significa que você pode começar a usar nossos serviços imediatamente após a adesão.\n\nOferecemos atendimento médico ilimitado, receitas\n\nAlém disso, temos uma ampla gama de benefícios, incluindo acesso a cursos gratuitos.'
-    );
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(
-      msg.from,
-      'COMO FUNCIONA?\nÉ muito simples.\n\n1º Passo\nFaça seu cadastro e escolha o plano que desejar.\n\n2º Passo\nApós efetuar o pagamento do plano escolhido você já terá acesso a nossa área exclusiva para começar seu atendimento na mesma hora.\n\n3º Passo\nSempre que precisar.'
-    );
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
+    for (const message of messages.options['1']) {
+      await simulateTyping(chat, 3000);
+      await client.sendMessage(msg.from, message);
+    }
   },
   '2': async (msg) => {
     const chat = await msg.getChat();
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(
-      msg.from,
-      '*Plano Individual:* R$22,50 por mês.\n\n*Plano Família:* R$39,90 por mês, inclui você mais 3 dependentes.\n\n*Plano TOP Individual:* R$42,50 por mês, com benefícios adicionais como\n\n*Plano TOP Família:* R$79,90 por mês, inclui você mais 3 dependentes.'
-    );
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
+    for (const message of messages.options['2']) {
+      await simulateTyping(chat, 3000);
+      await client.sendMessage(msg.from, message);
+    }
   },
   '3': async (msg) => {
     const chat = await msg.getChat();
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(
-      msg.from,
-      'Sorteio de em prêmios todo ano.\n\nAtendimento médico ilimitado 24h por dia.\n\nReceitas de medicamentos.'
-    );
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
+    for (const message of messages.options['3']) {
+      await simulateTyping(chat, 3000);
+      await client.sendMessage(msg.from, message);
+    }
   },
   '4': async (msg) => {
     const chat = await msg.getChat();
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(
-      msg.from,
-      'Você pode aderir aos nossos planos diretamente pelo nosso site ou pelo WhatsApp.\n\nApós a adesão, você terá acesso imediato.'
-    );
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(msg.from, 'Link para cadastro: https://site.com');
+    for (const message of messages.options['4']) {
+      await simulateTyping(chat, 3000);
+      await client.sendMessage(msg.from, message);
+    }
   },
   '5': async (msg) => {
     const chat = await msg.getChat();
-    await simulateTyping(chat, 3000);
-    await client.sendMessage(
-      msg.from,
-      'Se você tiver outras dúvidas ou precisar de mais informações, por favor, fale aqui nesse whatsapp ou visite nosso site: https://site.com.'
-    );
+    for (const message of messages.options['5']) {
+      await simulateTyping(chat, 3000);
+      await client.sendMessage(msg.from, message);
+    }
   },
 };
 
@@ -90,7 +78,7 @@ client.on('message', async (msg) => {
       const name = contact.pushname;
       await client.sendMessage(
         msg.from,
-        `Olá! ${name.split(' ')[0]}, sou o assistente virtual da empresa tal. Como posso ajudá-lo hoje? Por favor, digite uma das opções abaixo:\n\n1 - Como funciona\n2 - Valores dos planos\n3 - Benefícios\n4 - Como aderir\n5 - Outras perguntas`
+        messages.greetings.welcome.replace('{name}', name.split(' ')[0])
       );
     }
 
@@ -99,10 +87,7 @@ client.on('message', async (msg) => {
       await responses[msg.body](msg);
     } else if (!msg.body.match(/(menu|Menu|dia|tarde|noite|oi|Oi|Olá|olá|ola|Ola)/i)) {
       // Fallback para mensagens não reconhecidas
-      await client.sendMessage(
-        msg.from,
-        'Desculpe, não entendi. Por favor, digite uma das opções:\n\n1 - Como funciona\n2 - Valores dos planos\n3 - Benefícios\n4 - Como aderir\n5 - Outras perguntas'
-      );
+      await client.sendMessage(msg.from, messages.greetings.fallback);
     }
   } catch (error) {
     console.error('Erro ao processar mensagem:', error);
